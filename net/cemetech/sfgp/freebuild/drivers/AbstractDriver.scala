398dce449fc7a43654f093a356f8b2038848febc
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2015-02-17 03:42:44 -0500

    Overhaul of initDefaultSetting & checkSetting
    Now uses functional style instead of ugly loops

--- net/cemetech/sfgp/freebuild/drivers/AbstractDriver.scala
+++ net/cemetech/sfgp/freebuild/drivers/AbstractDriver.scala
@@ -1,6 +1,9 @@
 package net.cemetech.sfgp.freebuild.drivers
 
+import java.util.logging.Logger
 import org.python.util.PythonInterpreter
+
+
 import org.python.core._
 
 abstract class AbstractDriver {
@@ -8,66 +11,57 @@ abstract class AbstractDriver {
 	@scala.beans.BeanProperty
 	var settings: PyDictionary = new PyDictionary()
 	protected var console: PythonInterpreter = null
+	
+	val logger = Logger.getLogger("net.cemetech.sfgp.freebuild.drivers")
 
 	def mainloop(): Int
 
 	def setConsole(npc: PythonInterpreter): Unit = { console = npc }
 	def getConsole(): PythonInterpreter = { console }
 
-	def initDefaultSetting(hierarchy: Array[PyString], defaultValue: PyObject): PyObject = {
-		var curDepth: PyObject = settings
-		var i = 0 // this is ugly and should probably be rewritten in a more functional way
-		// but I'm not sure about the scoping, so it stays for now.
-		try {
-			while (i < hierarchy.length) {
-				curDepth = curDepth.asInstanceOf[PyDictionary].setdefault(
-					hierarchy(i),
-					if (i + 1 == hierarchy.length) {
-						defaultValue
-					} else {
-						new PyDictionary()
-					})
-				i = i + 1
-			}
-			curDepth
-		} catch {
-			case e: ClassCastException =>
-				System.err.println("settings hierarchy broken by non-dictionary type:")
-				System.err.print("\tinspected settings")
-				for (j <- 0 until i) {
-					System.err.printf("[%s]", hierarchy(j).__repr__().asString())
-				}
-				System.err.printf(" and found %s instead", curDepth.__repr__().asString())
-				e.printStackTrace()
-				defaultValue
+	def handleBrokenDictHierarchy(hierarchy:Array[PyString], i:Int, curDepth:PyObject, retVal:PyObject) = {
+		logger.warning(f"settings hierarchy broken by non-dictionary type:\n\tinspected settings${(0 until i).map( j => f"[${hierarchy(j).__repr__().asString()}%s]").mkString}%s  and found ${curDepth.__repr__().asString()}%s instead")		
+		retVal
+	}
+	
+	def recurseDictLayer(dict:PyObject, hierarchy:Array[PyString],depth:Int, dictProbe:(PyDictionary, Array[PyString], Int) => PyObject):Option[PyObject] = {
+		dict match {
+			case d:PyDictionary =>
+				Some(dictProbe(d, hierarchy, depth))
+			case _ =>
+				None
 		}
+	}
+	
+	def traverseDictByLayers(dict:PyObject, hierarchy:Array[PyString], depth:Int, dictProbe:(PyDictionary, Array[PyString], Int) => PyObject, default:PyObject):PyObject = {
+		if(depth == hierarchy.length){
+			dict
+		} else {
+			recurseDictLayer(dict, hierarchy, depth, dictProbe) match {
+				case Some(obj) => traverseDictByLayers(obj, hierarchy, depth + 1, dictProbe, default)
+				case None => handleBrokenDictHierarchy(hierarchy,depth,dict,default)
+			}
+			
+		}
+	}
+	
+	def initDefaultSetting(hierarchy: Array[PyString], defaultValue: PyObject): PyObject = {
+		traverseDictByLayers(settings, hierarchy, 0, {
+			(dict:PyDictionary, hierarchy:Array[PyString], i:Int) => 
+				dict.setdefault(hierarchy(i),
+						if(i + 1 == hierarchy.length){
+							defaultValue
+						} else {
+							new PyDictionary()
+						})
+			}, defaultValue)
 	}
 
 	def checkSetting(hierarchy: Array[PyString]): PyObject = {
-		var curDepth: PyObject = settings
-		var i = 0
-		try {
-			while (i < hierarchy.length) {
-				curDepth = curDepth.asInstanceOf[PyDictionary].get(hierarchy(i), Py.None)
-				i = i + 1
-			}
-			curDepth
-		} catch {
-			case e: ClassCastException =>
-				System.err.println("settings hierarchy broken by non-dictionary type:")
-				System.err.print("\tinspected settings")
-				for (j <- 0 until i) {
-					System.err.printf("[%s]", hierarchy(j).__repr__().asString())
-				}
-				System.err.printf(" and found %s instead", curDepth.__repr__().asString())
-				e.printStackTrace()
-				Py.None
-		}
+		traverseDictByLayers(settings, hierarchy, 0, {
+			(dict:PyDictionary, hierarchy:Array[PyString], i:Int) => dict.get(hierarchy(i), Py.None)
+		}, Py.None)
 	}
 
 	def cleanup(): Unit = { setConsole(null) }
-
-	//public void replaceSettings(PyDictionary newSettings){ settings = newSettings; }
-	//public PyDictionary getSettings(){ return settings; }
-
 }
